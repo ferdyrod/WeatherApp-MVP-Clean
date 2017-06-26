9e411b7ee0134363a810cd9744b919c04406dc8e
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2017-06-27 00:46:27 +0200

    Initial Commit

---
+++ app/src/main/java/com/ferdyrodriguez/weatherapp/WeatherCastActivity.java
@@ -0,0 +1,13 @@
+package com.ferdyrodriguez.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherCastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_cast);
+    }
+}
